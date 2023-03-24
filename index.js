957a1c46e9cb79dfb3aac027c688170e6c0be699
augustinebaidoo <augustine.baidoo@amalitech.org>
2023-03-24 04:26:33 +0000

    4th commit

--- index.js
+++ index.js
@@ -32,10 +32,36 @@ const resultDivs = document.querySelectorAll('.results_result')
 choiceButtons.forEach( button => {
     button.addEventListener('click', () => {
         const choiceName = button.dataset.choice;
-        const choice = CHOICES.find(choice => choice.name)
+        const choice = CHOICES.find(choice => choice.name == choiceName)
+        choose(choice)
     })    
 })
 
+function choose(choice) {
+    const aichoice = aiChoose ()
+    displayResults([choice,aichoice]) 
+}
+
+function aiChoose() {
+    const rand = Math.floor(Math.random() * CHOICES.length)
+    return CHOICES[rand]
+}
+
+function displayResults(results) {
+    resultDivs.forEach((resultDiv, idx) => {
+    setTimeout(() => {
+        resultDiv.innerHTML = `
+        <div class="choice ${results[idx].name}">
+        <img src="images/icon-${results[idx].name}.svg" alt="${results[idx].name}
+        " />
+        </div>
+        `
+    }, idx *  1000);
+    })
+
+    gameDiv.classList.toggle('hidden')
+    resultsDiv.classList.toggle('hidden')
+}
 
 
 //Show/Hide Rules
